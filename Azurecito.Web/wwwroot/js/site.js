2737dca228e8d7f4bb4c07ad1085b16b90c22fd7
Celu Taumos <celena.ark@gmail.com>
2024-06-21 19:11:59 -0300

    ultimos cambios

--- Azurecito.Web/wwwroot/js/site.js
+++ Azurecito.Web/wwwroot/js/site.js
@@ -1,4 +1,56 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const dropArea = document.getElementById("dragArea"),
+    dragText = dropArea.querySelector("h6"),
+    browseButton = dropArea.querySelector("#browseButton"),
+    fileInput = dropArea.querySelector("#fileInput"),
+    confirmButton = document.getElementById("confirmButton");
 
-// Write your JavaScript code.
+let file;
+
+browseButton.onclick = () => {
+    fileInput.click();
+}
+
+fileInput.addEventListener("change", function () {
+    file = this.files[0];
+    dropArea.classList.add("active");
+    viewFile();
+});
+
+dropArea.addEventListener("dragover", (event) => {
+    event.preventDefault();
+    dropArea.classList.add("active");
+    dragText.textContent = "Soltar para subir el archivo";
+});
+
+dropArea.addEventListener("dragleave", () => {
+    dropArea.classList.remove("active");
+    dragText.textContent = "Arrastre y suelte la imagen";
+});
+
+dropArea.addEventListener("drop", (event) => {
+    event.preventDefault();
+
+    file = event.dataTransfer.files[0];
+    viewFile();
+});
+
+function viewFile() {
+    let fileType = file.type;
+    let validExtensions = ["image/jpeg", "image/jpg", "image/png"];
+    if (validExtensions.includes(fileType)) {
+        let fileReader = new FileReader();
+        fileReader.onload = () => {
+            let fileURL = fileReader.result;
+            let imgTag = `<img src="${fileURL}" alt="image">`;
+            dropArea.innerHTML = imgTag;
+            confirmButton.style.display = "block";
+        }
+        fileReader.readAsDataURL(file);
+    } else {
+        alert("Este no es un archivo de imagen ");
+        dropArea.classList.remove("active");
+        dragText.textContent = "Arrastre y suelte para cargar la imagen";
+    }
+}
+
+confirmButton.addEventListener("click", () => { });
